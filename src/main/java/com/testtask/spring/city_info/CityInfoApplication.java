1cf7fda56c898ccfcdd724aeac766b07377d91ae
Liza Seledtsova <liza.seledtsova@icloud.com>
2020-09-25 12:17:42 +0300

    Start file

---
+++ src/main/java/com/testtask/spring/city_info/CityInfoApplication.java
@@ -0,0 +1,13 @@
+package com.testtask.spring.city_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testtask/spring/city_info/CityInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtask.spring.city_info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
